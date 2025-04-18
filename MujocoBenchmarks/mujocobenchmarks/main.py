93ab75d30ae5349ed1a7f6bb13165c091bf00b97
Leonard Papenmeier <leonard.papenmeier@gmail.com>
2025-04-18 09:30:27 +0200

    add lunarlander

--- MujocoBenchmarks/mujocobenchmarks/main.py
+++ MujocoBenchmarks/mujocobenchmarks/main.py
@@ -1,8 +1,8 @@
 import logging
-
 import numpy as np
 from bencherscaffold.bencher_pb2 import BenchmarkRequest, EvaluationResult
 from bencherscaffold.grcp_service import GRCPService
+from gym.envs.box2d import LunarLander
 
 from mujocobenchmarks.functions import func_factories
 
@@ -31,6 +31,34 @@ benchmark_bounds = {
 }
 
 
+def heuristic_controller(
+        state: np.ndarray,
+        x: np.ndarray
+) -> int:
+    angle_targ = state[0] * x[0] + state[2] * x[1]
+    if angle_targ > x[2]:
+        angle_targ = x[2]
+    if angle_targ < -x[2]:
+        angle_targ = -x[2]
+    hover_targ = x[3] * np.abs(state[0])
+
+    angle_todo = (angle_targ - state[4]) * x[4] - (state[5]) * x[5]
+    hover_todo = (hover_targ - state[1]) * x[6] - (state[3]) * x[7]
+
+    if state[6] or state[7]:
+        angle_todo = x[8]
+        hover_todo = -(state[3]) * x[9]
+
+    a = 0
+    if hover_todo > np.abs(angle_todo) and hover_todo > x[10]:
+        a = 2
+    elif angle_todo < -x[11]:
+        a = 3
+    elif angle_todo > +x[11]:
+        a = 1
+    return a
+
+
 class MujocoServiceServicer(GRCPService):
 
     def __init__(
@@ -43,16 +71,35 @@ class MujocoServiceServicer(GRCPService):
             request: BenchmarkRequest,
             context
     ) -> EvaluationResult:
-        assert request.benchmark in func_factory_map.keys(), "Invalid benchmark name"
         x = request.point.values
         x = np.array(x).reshape(1, -1)
-        # x is in [0, 1] space, we need to map it to the benchmark space
-        lb, ub = benchmark_bounds[request.benchmark]
-        x = lb + (ub - lb) * x
-        func_factory = func_factory_map[request.benchmark](None)
-        result = EvaluationResult(
-            value=-float(func_factory(x)[0].squeeze()),
-        )
+        if request.benchmark in func_factory_map.keys():
+            # x is in [0, 1] space, we need to map it to the benchmark space
+            lb, ub = benchmark_bounds[request.benchmark]
+            x = lb + (ub - lb) * x
+            func_factory = func_factory_map[request.benchmark](None)
+            result = EvaluationResult(
+                value=-float(func_factory(x)[0].squeeze()),
+            )
+        elif request.benchmark == 'lunarlander':
+            env = LunarLander()
+            total_reward = 0
+            steps = 0
+            s, info = env.reset()
+            while True:
+                a = heuristic_controller(s, x.squeeze())
+                s, r, terminated, info = env.step(a)
+                total_reward += r
+
+                steps += 1
+                if terminated:
+                    break
+            result = EvaluationResult(
+                value=total_reward
+            )
+        else:
+            raise ValueError("Invalid benchmark name")
+
         return result
 
 
