274041bd89c1b3d2607b76cdfa1853f4dc142b29
Leonard Papenmeier <leonard.papenmeier@gmail.com>
2025-05-14 13:22:20 +0200

    rm nevergrad for now, add mixed svm

--- SVMBenchmarks/svmbenchmarks/main.py
+++ SVMBenchmarks/svmbenchmarks/main.py
@@ -1,10 +1,11 @@
-import io
 import logging
 import lzma
 import os
 import tempfile
 import threading
+from typing import Optional, Callable, Tuple
 
+import math
 import numpy as np
 from bencherscaffold.protoclasses.bencher_pb2 import BenchmarkRequest, EvaluationResult
 from bencherscaffold.protoclasses.grcp_service import GRCPService
@@ -40,24 +41,6 @@ def download_slice_localization_data():
             out.write(f.read().decode("utf-8"))
 
         print(f"Downloaded slice_localization_data.csv")
-
-
-def _load_data():
-    """
-    _load_data()
-    -----------
-
-    This method is used to load data for CT slice localization. It downloads the data if necessary and processes it for further use.
-
-    :return: A tuple containing the features (X) and labels (y) of the CT data. X is a numpy array of shape (n_samples, n_features) and y is a numpy array of shape (n_samples,). The features
-    * and labels are scaled using MinMaxScaler.
-
-    Example usage:
-
-        >>> X, y = _load_data()
-    """
-
-    download_slice_localization_data()
     if not os.path.exists(os.path.join(directory_name, "CT_slice_X.npy")):
         data = np.genfromtxt(
             os.path.join(directory_name, "slice_localization_data.csv"),
@@ -69,9 +52,66 @@ def _load_data():
         np.save(os.path.join(directory_name, "CT_slice_y.npy"), y)
     X = np.load(os.path.join(directory_name, "CT_slice_X.npy"))
     y = np.load(os.path.join(directory_name, "CT_slice_y.npy"))
+    return X, y
+
+
+def load_data_388():
+    """
+    _load_data()
+    -----------
+
+    This method is used to load data for CT slice localization. It downloads the data if necessary and processes it for further use.
+
+    :return: A tuple containing the features (X) and labels (y) of the CT data. X is a numpy array of shape (n_samples, n_features) and y is a numpy array of shape (n_samples,). The features
+    * and labels are scaled using MinMaxScaler.
+
+    Example usage:
+
+        >>> X, y = load_data_388()
+    """
+
+    X, y = download_slice_localization_data()
     X = MinMaxScaler().fit_transform(X)
     y = MinMaxScaler().fit_transform(y.reshape(-1, 1)).squeeze()
-    return X, y
+    idxs = RandomState(388).choice(np.arange(len(X)), min(500, len(X)), replace=False)
+    half = len(idxs) // 2
+    x_train = X[idxs[:half]]
+    x_test = X[idxs[half:]]
+    y_train = y[idxs[:half]]
+    y_test = y[idxs[half:]]
+
+    return x_train, y_train, x_test, y_test
+
+
+def load_data_53(
+        n_features: Optional[int] = None,
+):
+    X, y = download_slice_localization_data()
+    X -= X.min(axis=0)
+    X = X[:, X.max(axis=0) > 1e-6]  # Throw away constant dimensions
+    X = X / (X.max(axis=0) - X.min(axis=0))
+    X = 2 * X - 1
+    assert X.min() == -1 and X.max() == 1
+
+    # Standardize targets
+    y = (y - y.mean()) / y.std()
+
+    # Only keep 10,000 data points and n_features features
+    shuffled_indices = np.random.RandomState(0).permutation(X.shape[0])[:10_000]  # Use seed 0
+    X, y = X[shuffled_indices], y[shuffled_indices]
+
+    if n_features is not None:
+        # Use Xgboost to figure out feature importances and keep only the most important features
+        xgb = XGBRegressor(max_depth=8).fit(X, y)
+        inds = (-xgb.feature_importances_).argsort()
+        X = X[:, inds[:n_features]]
+
+    # Train/Test split on a subset of the data
+    train_n = int(math.floor(0.50 * X.shape[0]))
+    train_x, train_y = X[:train_n], y[:train_n]
+    test_x, test_y = X[train_n:], y[train_n:]
+
+    return train_x, train_y, test_x, test_y
 
 
 class SvmServiceServicer(GRCPService):
@@ -96,18 +136,13 @@ class SvmServiceServicer(GRCPService):
             self
     ):
         super().__init__(port=50058)
-        self.data_initialized = False
+        self.data_initialized = None
 
     def initialize_data(
-            self
+            self,
+            data_loader: Callable[[], Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]] = load_data_388
     ):
-        self.X, self.y = _load_data()
-        idxs = RandomState(388).choice(np.arange(len(self.X)), min(500, len(self.X)), replace=False)
-        half = len(idxs) // 2
-        self._X_train = self.X[idxs[:half]]
-        self._X_test = self.X[idxs[half:]]
-        self._y_train = self.y[idxs[:half]]
-        self._y_test = self.y[idxs[half:]]
+        self._X_train, self._y_train, self._X_test, self._y_test = data_loader()
 
     def evaluate_point(
             self,
@@ -127,11 +162,21 @@ class SvmServiceServicer(GRCPService):
 
         Please note that this method assumes that the benchmark name in the request is "svm". If the benchmark name is different, an assertion error will occur.
         """
-        assert request.benchmark.name == "svm", "Invalid benchmark name"
+        valid_benchmark_names = ['svm', 'svmmixed']
+        assert request.benchmark.name in valid_benchmark_names, f"Invalid benchmark name: {request.benchmark.name}. Expected one of {valid_benchmark_names}"
+        if request.benchmark.name == 'svmmixed':
+            loader = load_data_53
+        elif request.benchmark.name == 'svm':
+            loader = load_data_388
+        else:
+            raise ValueError(
+                f"Invalid benchmark name: {request.benchmark.name}. Expected one of {valid_benchmark_names}"
+            )
+
         with lock:
-            if not self.data_initialized:
-                self.initialize_data()
-                self.data_initialized = True
+            if self.data_initialized is None or self.data_initialized != loader:
+                self.initialize_data(loader)
+                self.data_initialized = loader
 
         x = request.point.values
         x = np.array(x).squeeze()
